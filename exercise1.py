2b4b9401090886b4a16d80d4702af2f59a4464fb
kating26 <kating26@gmail.com>
2015-12-13 17:19:54 -0500

    Assignment 2 Removal of Inforum changes

--- exercise1.py
+++ exercise1.py
@@ -44,7 +44,7 @@ def selection(t, f):
 
     Example:
     > R = [["A", "B", "C"], [1, 2, 3], [4, 5, 6]]
-    ># Define function f that returns True iff
+    ># Define function f that returns True if
     > # the last element in the row is greater than 3.
     > def f(row): row[-1] > 3
     > select(R, f)
@@ -58,44 +58,6 @@ def selection(t, f):
         if f(t[a]):
             new_table.append(t[a])
     return new_table
-
-
-#From Assignment 2
-def union(table1, table2):
-    """
-    Perform the union set operation on tables, table1 and table2.
-    :param table1: a table (a List of Lists)
-    :param table2: a table (a List of Lists)
-    :return: the resulting table
-    :raises: MismatchedAttributesException:
-        if tables t1 and t2 don't have the same attributes
-    """
-    new_table = []
-
-    # Schema match check:
-    if table1[0] == table2[0]:
-        # Append header row
-        new_table.append(table1[0])
-
-        # Matching table1 rows to table2
-        for t1_row in table1[1:]:
-            if t1_row not in table2 or t1_row not in new_table:
-                new_table.append(t1_row)
-
-        # Matching table2 rows to table1
-        for t2_row in table2[1:]:
-            if t2_row not in table1 or t2_row not in new_table:
-                new_table.append(t2_row)
-    else:
-        raise MismatchedAttributesException("Bad Schema.")
-
-    # 2015-11-06 update: if no common rows, should return nothing
-    if len(new_table) < 2:
-        # new_table = []
-        return None
-
-    return new_table
-# End of Assignment 2
 
 # Work from inforum
 t = EMPLOYEES = [["Surname", "FirstName", "Age", "Salary"],
