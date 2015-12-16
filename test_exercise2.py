7c54e1591bf87e45f5ead92a1e14d29f647db760
kating26 <kating26@gmail.com>
2015-12-15 19:12:25 -0500

    Test Cases - Valid Date and Passport Format

--- test_exercise2.py
+++ test_exercise2.py
@@ -3,8 +3,7 @@
 """ Module to test papers.py  """
 
 __author__ = 'Hana Nagel, Liana Sukaisyan and Katherine Ing'
-__email__ = "hana.nagel@mail.utoront.ca; liana.sukiasyan@mail.utoronto.ca; k.ing@mail.utoronto.ca"
-
+__email__ = "hana.nagel@mail.utoronto.ca; liana.sukiasyan@mail.utoronto.ca; k.ing@mail.utoronto.ca"
 __copyright__ = "2015 Hana Nagel, Liana Sukaisyan and Katherine Ing"
 __license__ = "MIT License"
 
@@ -13,7 +12,7 @@ __status__ = "Prototype"
 # imports one per line
 import pytest
 import os
-from exercise2 import decide
+from exercise2 import *
 
 DIR = "test_jsons/"
 os.chdir(DIR)
@@ -25,4 +24,25 @@ def test_returning():
     """
     assert decide("test_returning_citizen.json", "countries.json") ==\
         ["Accept", "Accept", "Quarantine"]
+
+def test_valid_date_format():
+    """
+    Test if the date is valid for a visitor visa
+    """
+    assert (valid_date_format('April 30, 2020')) == False
+    assert (valid_date_format('')) == False
+    assert (valid_date_format('1967-30-04')) == True
+    assert (valid_date_format('2016-30-04')) == True
+
+def test_valid_visa_format():
+    """
+    Test if the visa code is valid
+    """
+    assert (valid_passport_format('KG65G-239FM-3D9G3-4K4R3-3LFK3')) == True
+    assert (valid_passport_format('')) == False
+    assert (valid_passport_format('@#$$#-@$#$3-@$#$@-@#$@$-^%$^%')) == False
+    assert (valid_passport_format('KDSJ-SKJF3-SK234-DK12')) == False
+    assert (valid_passport_format('416-444-4444')) == False
+    assert (valid_passport_format('KG65G_239FM_3D9G3_DK4R3_3LFK3')) == False
+    assert (valid_date_format('happy')) == False
 
